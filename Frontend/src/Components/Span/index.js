b48a71eb218545e32f1d60d661576c2e83530509
Jason Matthias <jpmatthias90@gmail.com>
2019-08-23 14:05:29 -0700

    Add styled span component

---
+++ Frontend/src/Components/Span/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Span = styled.span`
+  font-size: ${props => (props.fontSize || '1em')};
+  color: ${props => (props.color ? props.color : props.theme.color)};
+`;
+
+export default Span;
