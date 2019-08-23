fd4dd400f65193722659bc97d218c19d66b56569
Jason Matthias <jpmatthias90@gmail.com>
2019-08-23 13:58:10 -0700

    Set up base express app

---
+++ Server/app.js
@@ -0,0 +1,46 @@
+/** Express app for Synapsefi takehom. */
+
+
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+const router = express.Router();
+
+app.use(express.json());
+app.use(cors());
+
+// add logging system
+
+app.use(morgan("tiny"));
+
+const userRoutes = require("./routes/users");
+const loginRoutes = require("./routes/login");
+
+app.use("/users", userRoutes);
+app.use("/login", loginRoutes);
+
+/** 404 handler */
+
+app.use(function (req, res, next) {
+  const err = new Error("Not Found");
+  err.status = 404;
+
+  // pass the error to the next piece of middleware
+  return next(err);
+});
+
+/** general error handler */
+
+app.use(function (err, req, res, next) {
+  // if (err.stack) console.log(err.stack);
+
+  res.status(err.status || 500);
+  return res.json({
+    message: err.message
+  });
+});
+
+
+module.exports = app;
