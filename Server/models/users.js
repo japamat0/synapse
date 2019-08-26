09d6193449d99589e9f6f6704e433c1aa9beb4b1
Jason Matthias <jpmatthias90@gmail.com>
2019-08-25 22:52:54 -0700

    Add user methods for app login, synapse oAuth

--- Server/models/users.js
+++ Server/models/users.js
@@ -6,16 +6,69 @@
 
 const client = require('../lib/synapseClient');
 const fs = require('fs');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const usersJSON = require('../data/users.json');
 
 class User {
-  static async getUser(userId) {
+  static async getSynapseUser(userId) {
     let user = await client.getUser(userId);
     return user;
   }
 
+  static async linkBankAccounts(body) {
+    let user = await client.getUser('5d5de6dda63ec2309c7aa2e0');
+    let res = await user.createNode(body);
+    return res;
+  }
+
+  static async appLogin(body) {
+    console.log(`in user model, body: `, body);
+    
+    const { username, password } = body;
+    const user = usersJSON[username];
+    if (user) {
+      console.log(`user exists in JSON file`);
+      
+      const isValid = await bcrypt.compare(password, user.password);
+      if (isValid) {
+        console.log(`user has been validated`);
+        
+        return ({
+          username,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          synapseId: user.synapseId,
+          email: user.email,
+          phone: user.phone,
+          DOB: user.DOB,
+        });
+      } else {
+        const invalidPass = new Error("Invalid Credentials.");
+        invalidPass.status = 401;
+        throw invalidPass;
+      }
+    } else {
+      const invalidUser = new Error(`Cannot find username: ${username}.`);
+      invalidUser.status = 404;
+      throw invalidUser;
+    }
+  }
+
+  static async oAuth(userId) {
+    const user = await this.getSynapseUser(userId);
+    const { refresh_token } = user.body;
+    const oAuthKeys = await user._oauthUser({ refresh_token });
+    console.log(oAuthKeys);
+    
+    return oAuthKeys;
+  }
+
   static async getAuthKey(userId) {
-    let user = await client.getUser(userId);
+    let user = await client.getUser(userId); // pulled from app (db/JSON file)
+    console.log(user);
+    
     const newFingerprint = 'static_pin';
     const device2FA = '901.111.1111';
     let res = await user.registerNewFingerprint(newFingerprint);
@@ -27,7 +80,7 @@ class User {
       "type": "DEPOSIT-US",
       "info": {
         "nickname": "My Deposit Account",
-        "document_id": ""
+        "document_id": user.body.documents[0].id
       }
     };
     let verified = await user.verifyFingerprint2FA(newFingerprint, '123456');
