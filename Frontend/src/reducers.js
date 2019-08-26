a849fc2de2c691c3d24ca32bea0467e4bdf1d8a0
Jason Matthias <jpmatthias90@gmail.com>
2019-08-25 22:53:57 -0700

    Add login reducer to global reducers

--- Frontend/src/reducers.js
+++ Frontend/src/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import appReducer from './Containers/App/reducer';
+import loginReducer from './Containers/LoginPage/reducer';
 
 const rootReducer = combineReducers({
   global: appReducer,
+  login: loginReducer,
 });
 
 export default rootReducer;
