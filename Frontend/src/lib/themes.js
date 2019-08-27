2f0f3b70e8197b99cfd0cb4331247d8a7703db53
Jason Matthias <jpmatthias90@gmail.com>
2019-08-26 17:38:51 -0700

    Adjust global theme colors

--- Frontend/src/lib/themes.js
+++ Frontend/src/lib/themes.js
@@ -1,13 +1,15 @@
 const light = {
   color: '#000',
   bg: '#fff',
-  accent: '#61ba9e'
+  accent: 'rgba(97, 186, 158)',
+  label: 'rgb(100, 100, 100)'
 }
 
 const dark = {
   color: '#fff',
-  bg: '#000',
-  accent: '#000fff'
+  bg: 'rgba(54, 61, 61, 0.75)',
+  accent: 'rgba(97, 186, 158, 0.75)',
+  label: 'rgb(161, 161, 161)'
 }
 
 export { light, dark };
