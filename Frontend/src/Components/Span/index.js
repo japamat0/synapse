19d4bf2d1e967c797b58fb8d01656e669532f521
Jason Matthias <jpmatthias90@gmail.com>
2019-08-26 17:51:13 -0700

    Change default span color to theme label

--- Frontend/src/Components/Span/index.js
+++ Frontend/src/Components/Span/index.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Span = styled.span`
   font-size: ${props => (props.fontSize || '1em')};
-  color: ${props => (props.color ? props.color : props.theme.color)};
+  color: ${props => (props.color ? props.color : props.theme.label)};
 `;
 
 export default Span;
