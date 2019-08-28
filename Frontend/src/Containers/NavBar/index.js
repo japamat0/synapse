b0ac1fe3e4d6bbc011501090f754cda826c478c2
Jason Matthias <jpmatthias90@gmail.com>
2019-08-28 02:46:34 -0700

    Change navbar based on logged in or out

--- Frontend/src/Containers/NavBar/index.js
+++ Frontend/src/Containers/NavBar/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
 
 import Icon from '../../Components/Icon';
 import Span from '../../Components/Span';
 import Button from '../../Components/Button';
+import { createStructuredSelector } from 'reselect';
+import { makeSelectAppTheme, makeSelectUsername } from '../App/selectors';
+import { logoutUser } from '../LoginPage/actions';
+import { compose } from 'redux';
 
-const propTypes = {};
-
-const defaultProps = {};
 
 const OuterWrapper = styled.div`
   padding: 0.75em;
@@ -32,29 +32,49 @@ const InnerWrapper = styled.div`
   align-items: center;
 `;
 
-export default class NavBar extends React.Component {
+class NavBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+
 
   render() {
     return (
       <OuterWrapper>
         <InnerWrapper>
           <GroupWrapper>
-            <Icon name="logo" height={48} width={48} />
-            <Span fontSize="1.75em">Bankly</Span>
+            <Icon name="logo" fill={this.props.theme.color} height={48} width={48} />
+            <Span fontSize="1.75em" color ={this.props.theme.color}>Bankly</Span>
           </GroupWrapper>
-          <GroupWrapper>
-            <Button link="/login">login</Button>
-            <Button link="/register">register</Button>
-          </GroupWrapper>
+          {this.props.username ? (
+            <Button handleRoute={this.props.logout}>Log Out</Button>
+            ) : (
+            <GroupWrapper>
+              <Button link="/login">login</Button>
+              <Button link="/register">register</Button>
+            </GroupWrapper>
+          )}
         </InnerWrapper>
       </OuterWrapper>
     );
   }
 }
 
- NavBar.propTypes = propTypes;
- NavBar.defaultProps = defaultProps;
+
+export const mapStateToProps = createStructuredSelector({
+  theme: makeSelectAppTheme(),
+  username: makeSelectUsername(),
+})
+
+export const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch(logoutUser()),
+})
+
+const connected = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)
+
+export default compose(connected)(NavBar)
