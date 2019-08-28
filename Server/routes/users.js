010b9e5c9e43ee11beb8cd342e9063af01ba41f2
Jason Matthias <jpmatthias90@gmail.com>
2019-08-28 00:35:51 -0700

    Add register user route

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -4,22 +4,9 @@ const express = require("express");
 const router = express.Router();
 const User = require('../models/users');
 
-/** GET / => {users: [user, ...]} */
-
-router.get("/", async function(req, res, next) {
-  try {
-    console.log('landed in the routes');
-    
-    let user = await User.getUser('5d5de6dda63ec2309c7aa2e0');
-    return res.json({ user, });
-  } catch (err) {
-    return next(err);
-  }
-});
 
 router.get("/availability", async function(req, res, next) {
   try {
-    
     const available = await User.checkAvailability(req.query.username);
     return res.json({ isAvailable: available });
   } catch (err) {
@@ -29,9 +16,16 @@ router.get("/availability", async function(req, res, next) {
 
 router.post("/", async function(req, res, next) {
   try {
-    let user = await User.appRegisterUser(req.body.user);
-    return res.status(201).json(user);
+    delete req.body._token;
+    const user = await User.registerSynapse(JSON.stringify(req.body.synapseBody));
+    await User.registerApp({
+      ...req.body.appBody,
+      synapseId: user.id
+    })
+    return res.status(201).json(user.body);
   } catch (err) {
+    console.log('user model err', err);
+    
     return next(err);
   }
 });
