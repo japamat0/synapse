33e7c6ccc1f76ef008591ee3fa144a4d04e3872a
Jason Matthias <jpmatthias90@gmail.com>
2019-08-28 02:37:40 -0700

    Add accounts route to server

--- Server/routes/accounts.js
+++ Server/routes/accounts.js
@@ -5,12 +5,12 @@ const router = express.Router();
 const User = require('../models/users');
 const { authRequired } = require('../middleware/auth');
 
-
+/**
+ * Uses token to get synapse ID, uses model to request that resource
+ */
 router.get("/", authRequired, async function(req, res, next) {
   try {
     const accounts = await User.getAccounts(req.synapseId);
-    console.log(accounts);
-    
     return res.json(accounts);
   } catch (err) {
     return next(err);
