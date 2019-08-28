e6c904c83a75813f5bf23383640824bc167ab222
Jason Matthias <jpmatthias90@gmail.com>
2019-08-28 02:43:59 -0700

    Add accounts page sagas/reducer to app

--- Frontend/src/reducers.js
+++ Frontend/src/reducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import appReducer from './Containers/App/reducer';
 import loginReducer from './Containers/LoginPage/reducer';
 import registerReducer from './Containers/RegisterPage/reducer';
+import accountsReducer from './Containers/AccountsPage/reducer';
 
 const rootReducer = combineReducers({
   global: appReducer,
   login: loginReducer,
   register: registerReducer,
+  accounts: accountsReducer,
 });
 
 export default rootReducer;
--- Frontend/src/rootSaga.js
+++ Frontend/src/rootSaga.js
@@ -1,10 +1,16 @@
 import { all } from 'redux-saga/effects';
 import { loginSagas } from './Containers/LoginPage/saga';
 import { registerSagas } from './Containers/RegisterPage/saga';
+import { accountsSagas } from './Containers/AccountsPage/saga';
 
+
+/**
+ * Combine all sagas
+ */
 export default function* rootSaga() {
   yield all([
     ...loginSagas,
     ...registerSagas,
-  ])
+    ...accountsSagas,
+  ]);
 }
